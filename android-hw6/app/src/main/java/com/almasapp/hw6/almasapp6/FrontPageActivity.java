1986a08e8197147f94db377f064abf1485f368e4
jeajjr <jeajjr@gmail.com>
2015-02-23 01:42:33 -0500

    Added action bar items to FrontPage.

--- android-hw6/app/src/main/java/com/almasapp/hw6/almasapp6/FrontPageActivity.java
+++ android-hw6/app/src/main/java/com/almasapp/hw6/almasapp6/FrontPageActivity.java
@@ -1,24 +1,30 @@
 package com.almasapp.hw6.almasapp6;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class FrontPageActivity extends ActionBarActivity {
+
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_front_page);
+
+        actionBar = getSupportActionBar();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_activity_front_page, menu);
         return true;
     }
 
@@ -30,9 +36,15 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_always_shown:
+                Toast.makeText(this, getResources().getString(R.string.front_page_action_always), Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_overflow:
+                Toast.makeText(this, getResources().getString(R.string.front_page_action_overflow), Toast.LENGTH_LONG).show();
+                return true;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
